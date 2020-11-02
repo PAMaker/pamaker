340a24235ad71e82f2bb4e762ee6d8636c6ad1af
ByeonYujin <byj0773@gmail.com>
2020-11-02 16:34:09 +0900

    header, footer 수정

--- web/www/lib/ptemplate.js
+++ web/www/lib/ptemplate.js
@@ -15,6 +15,7 @@ module.exports = {
    
          <link rel="stylesheet" href="gallery.css">
          <link rel="stylesheet" href="calendar.css">
+         <link rel="stylesheet" href="nav.css">
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
       
        
@@ -34,16 +35,31 @@ module.exports = {
 </head>
 <body>
 <header>
-  <nav>
-    <div class="nav-wrapper">
-      <a href="#!" class="brand-logo center"><i class="material-icons">linked_camera</i>22세기사진관</a>
-      <ul id="nav-mobile" class="left">
-        <li><a class="material-icons" href="/first.html">keyboard_arrow_left</a></li>
-      </ul>
-      <ul id="nav-mobile" class="right">
-      <li><a href="sass.html"><i class="material-icons">search</i></a></li></ul>
-    </div>
-  </nav>
+<nav>
+  <div class="nav-wrapper">
+    <a href="" class="brand-logo black-text"
+      ><img src="logo.png" style="width: 80px" alt="" />22세기 사진관</a
+    >
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <li><a href="sass.html">Sass</a></li>
+      <li><a href="badges.html">Components</a></li>
+      <li><a href="collapsible.html">JavaScript</a></li>
+    </ul>
+
+    <ul id="nav-mobile" class="left">
+      <li>
+        <a class="material-icons" href="first.html"
+          >keyboard_arrow_left</a
+        >
+      </li>
+    </ul>
+    <ul id="nav-mobile" class="right">
+      <li>
+        <a href="page/sass.html"><i class="material-icons">search</i></a>
+      </li>
+    </ul>
+  </div>
+</nav>
 </header>
 <div class="col s12 l6 offset-l1 center-align">
 <h4 style="margin-top: 70px; color: grey;">반갑습니다 작가님!</h4>
@@ -61,21 +77,22 @@ module.exports = {
 </div>
 </header>
 
-  <footer class="page-footer">
-  <nav>
-    <div class="nav-wrapper">
-      <ul id="nav-mobile" class="center">
-        <li><a class="material-icons" href="first.html">home</a></li>
-        <li><a class="material-icons" href="fav">favorite_border</a></li>
-        <li><a class="material-icons" href="pchat">chat</a></li>
-      </ul>
+  <footer class="page-footer" style="background-color:#242424">
+    <nav>
+      <div class="nav-wrapper">
+        <ul id="nav-mobile" class="center">
+          <li><a class="material-icons" href="first.html">home</a></li>
+          <li><a class="material-icons" href="fav">favorite_border</a></li>
+          <li><a class="material-icons" href="pchat">chat</a></li>
+        </ul>
         <ul id="nav-mobile" class="right">
            <li><a class="material-icons" href="pmypage">account_circle</a></li>
         </ul>
-      </ul>
-    </div>
+      </div>
     </nav>
   </footer>
+
+  
 
 
 </body>
