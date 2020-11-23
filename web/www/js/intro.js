cb91ea213907d27613f2698f33425e06ee32b9ff
ByeonYujin <byj0773@gmail.com>
2020-11-23 23:51:49 +0900

    color test

--- web/www/js/intro.js
+++ web/www/js/intro.js
@@ -1,8 +1,19 @@
-$(document).ready(function(){
-    $(".carousel").carousel();
-  })
+// $(document).ready(function(){
+//     $(".carousel").carousel();
+//   })
 
-document.addEventListener('DOMContentLoaded', function() {
-var elems = document.querySelectorAll('.carousel');
-var instances = M.Carousel.init(elems, options);
-});
+// document.addEventListener('DOMContentLoaded', function() {
+// var elems = document.querySelectorAll('.carousel');
+// var instances = M.Carousel.init(elems, options);
+// });
+
+// var survey_trigger = document.getElementsByClassName("banner");
+
+// function popup(){
+//   var url = "/survey.html";
+//   var name = "popup test";
+//   var option = "width = 500, height = 500, top = 100, left = 200, location = no"
+//   window.open(url, name, option);
+// }
+
+// survey_trigger.addEventListener('click', popup())
