0e475c7391dc8f23780904a2bfa1fde7b2ff8c05
ByeonYujin <byj0773@gmail.com>
2020-11-26 12:38:01 +0900

    first.html 수정

--- web/www/lib/template.js
+++ web/www/lib/template.js
@@ -19,6 +19,7 @@ module.exports = {
         <link rel="stylesheet" type="text/css" href="first.css">
         <link rel="stylesheet" type="text/css" href="nav.css">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <script src="navbarLink.js></script>
     <style>
     html,body{
       width:100%;
